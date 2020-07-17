26bc2e4cdad83a5914464437b849c978d717e6b5
DiegoOTdC <studioartdiego@gmail.com>
2020-07-17 12:34:37 +0200

    register 2 GET routes for user and image

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const app = express();
+const imageRouter = require("./routers/image");
+const userRouter = require("./routers/user");
+
 const PORT = process.env.PORT || 4000;
+
+app.use("/images", imageRouter);
+app.use("/users", userRouter);
+
 app.listen(PORT, () => console.log(`Listening on :${PORT}`));
